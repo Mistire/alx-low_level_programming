08437823a28e869db9167182cf4e3b9490472138
Mistire <mdwithgod@gamail.com>
2023-09-07 16:26:24 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Retrun: Always 0 (SUCCESS)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
