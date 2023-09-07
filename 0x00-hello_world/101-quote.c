dce8e61bdb03e67306427396c4b41591f2df8f0f
Mistire <mdwithgod@gamail.com>
2023-09-07 17:15:14 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	int i = 0;
+
+	while (quote[i] != '\0')
+	{
+		putchar(quote[i]);
+		i++;
+	}
+
+	return (0);
+}
