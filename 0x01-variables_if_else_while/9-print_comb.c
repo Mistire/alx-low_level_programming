16b5d3c1cc319c5cf24db8cc9ea3f7a64a929237
Mistire <mdwithgod@gamail.com>
2023-09-08 09:06:37 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+
+		if (i == 57)
+		{
+			break;
+		}
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
