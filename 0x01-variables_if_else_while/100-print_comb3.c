10aba6d9199689f40b433a28ef01766260e0d372
Mistire <mdwithgod@gamail.com>
2023-09-08 09:47:25 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+
+		for (j = 49; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i == 56 && j == 57)
+				break;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
