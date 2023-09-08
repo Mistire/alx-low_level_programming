3cbc897732fce6a7108ca9bfa19c7ed7b98d4c49
Mistire <mdwithgod@gamail.com>
2023-09-08 16:31:41 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				
+
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
@@ -33,4 +33,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-}	
+}
