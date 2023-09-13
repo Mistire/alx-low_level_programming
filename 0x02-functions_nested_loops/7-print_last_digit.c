197635e0abd07f0275b1961cca2a672f8b9cbc35
Mistire <mdwithgod@gamail.com>
2023-09-13 10:41:24 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * Return: value of the last digit
+ * @n: value
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+
+	_putchar('\n');
+}
