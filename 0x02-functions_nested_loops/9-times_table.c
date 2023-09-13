c27f00a7759c4bd4f56780dd221994bdde90129e
Mistire <mdwithgod@gamail.com>
2023-09-13 13:11:05 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#inlcude "main.h"
+/**
+ * times_table - prints the 9 times table with 0
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(i * j);
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		_putchar('\n');
+	}
+}
