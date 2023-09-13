da9cb6ff512459063ad3b6aa4391bdc51947c627
Mistire <mdwithgod@gamail.com>
2023-09-13 09:05:39 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * print_alphabet - Prints alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+
+}
