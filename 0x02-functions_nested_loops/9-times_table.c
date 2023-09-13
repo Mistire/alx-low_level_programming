cf53d04db9c9944bdf6d0ca5016e1dc4e6adbd31
Mistire <mdwithgod@gamail.com>
2023-09-13 15:10:57 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,21 +13,24 @@ void times_table(void)
 		{
 			k = i * j;
 
-			if (k >= 0 && k < 10)
+			if (j == 0)
 			{
 				_putchar(k + '0');
+			}
 
-				if (j < 9)
+			else if (k < 10)
+			{
+				if (j != 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
+				_putchar(k + '0');
 			}
 
-			else if (k != 9)
+			else if (k > 10)
 			{
-
 				if (j != 9)
 				{
 					_putchar(',');
@@ -36,9 +39,7 @@ void times_table(void)
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
 			}
-
 		}
-
 		_putchar('\n');
 	}
 }
