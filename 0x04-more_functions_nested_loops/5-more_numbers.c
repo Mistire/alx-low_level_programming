e15994ba8c65c566b86c908aa0d96830cf3311e4
Mistire <mdwithgod@gamail.com>
2023-09-15 17:29:29 +0000

    more_number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j > 9)
+			if (j >= 10)
 			{
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
 			}
-			else
-			{
-			_putchar(j + '0');
-			}
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
