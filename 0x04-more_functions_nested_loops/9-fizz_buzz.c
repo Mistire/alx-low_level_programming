632b51e730b26b14c86bb7a312d7d5127633ed99
Mistire <mdwithgod@gamail.com>
2023-09-15 17:46:09 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("%s", Fizz);
+			printf("%s", "Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s", Buzz);
+			printf("%s", "Buzz");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("%s", FizzBuzz);
+			printf("%s", "FizzBuzz");
 		}
 		else
 		{
