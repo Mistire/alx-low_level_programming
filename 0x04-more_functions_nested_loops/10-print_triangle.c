f49379be7d11e22cd50c16358c650638bc1c7bb1
Mistire <mdwithgod@gamail.com>
2023-09-15 14:31:36 +0000

    trianlge

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,16 +16,15 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size - 1; j++)
+			for (j = i; j < size - 1; j++)
 			{
 				_putchar(' ');
 			}
-			for (k = i; k < i + 1; k++)
+			for (k = 0; k < i + 1; k++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
