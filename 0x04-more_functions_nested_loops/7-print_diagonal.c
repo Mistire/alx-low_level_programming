8ebcc9e5673dc7f05b6eafe898da8fb2b7b8b991
Mistire <mdwithgod@gamail.com>
2023-09-15 16:32:16 +0000

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (n < 1)
 	{
 		_putchar('\n');
 	}
@@ -16,7 +16,7 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < i + 1; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
