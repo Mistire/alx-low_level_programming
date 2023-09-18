9431bec3aa17d28add02502f7f8d6176b5ef75ed
Mistire <mdwithgod@gamail.com>
2023-09-18 17:39:10 +0000

    every

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string
  * @str: character
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i+=2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (i % 2 == 0)
+		{
 		_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
