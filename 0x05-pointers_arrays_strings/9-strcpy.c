d12eb9a39395066b8dd7d9d860914c95937ab0c7
Mistire <mdwithgod@gamail.com>
2023-09-18 18:38:26 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcpy -  copies the string pointed to by src
+ * @dest: strign1
+ * @src: string2
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
