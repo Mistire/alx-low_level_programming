fad8e9b21da0b07b00892c3f53028b83e3331e52
Mistire <mdwithgod@gamail.com>
2023-09-18 17:21:43 +0000

    half

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - a function that prints half of a string,
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = strlen(str);
+	int n = (len - 1) / 2;
+
+	if (len % 2 != 0)
+	{
+		for (i = len - n; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
