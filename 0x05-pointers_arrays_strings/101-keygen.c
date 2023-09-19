9fa6afee7f0096813c4ef1c6ec11bc6fd342e4f0
Mistire <mdwithgod@gamail.com>
2023-09-19 05:36:16 +0000

    crack

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,39 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
+#include <string.h>
+
+#define PASSWORD_LENGTH 100
+#define CHECKSUM 2772
+
+
 
 /**
- * randomChar - Generate a random character from 'A' to 'Z'
- * Return: Random character
- */
-char randomChar(void)
+* generatePassword - A function that generate a random password an place it
+* inside an allucated variable named password
+* @_sum: A pointer to a variable that keep tracks genretad password
+* ascii value sum.
+* Return: The allocated char* password
+*/
+char *generatePassword(int *_sum) 
 {
-	return ('A' + rand() % 26);
+	const char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *password = malloc(PASSWORD_LENGTH + 1);
+	int i;
+
+	srand(time(NULL));
+	for (i = 0; *_sum < CHECKSUM - (127); ++i) {
+		password[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+		*_sum += (int) password[i];
+	}
+	password[i] = '\0';
+	return password;
 }
 
 /**
- * main - Generate a random password
- * Return: 0 on success
+ * main - Entry point
+ * Description: A program that generetes a valid key for the program 101-crackme
+ * Return: 0
  */
 int main(void)
 {
-    /* Seed the random number generator with the current time */
-	srand(time(NULL));
+	int sum = 0;
+	char *password = generatePassword(&sum);
 
-    /* Set the desired password length */
-	int passwordLength = 12;
-	int i;
+	char *difference = malloc(2);
+	difference[0] = CHECKSUM - sum;
+	difference[1] = '\0';
+	strcat(password, difference);
 
-	printf("Generated Password: ");
+	printf("%s", password);
 
-	for (i = 0; i < passwordLength; i++)
-	{
-		char randomCharacter = randomChar();
-
-		printf("%c", randomCharacter);
-	}
-
-	printf("\n");
-
+	free(password);
+	free(difference);
 	return (0);
 }
