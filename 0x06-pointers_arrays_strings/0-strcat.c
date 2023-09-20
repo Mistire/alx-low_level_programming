e46b4b478ba619b7247b2f4cb56eb649a710d622
Mistire <mdwithgod@gamail.com>
2023-09-20 23:24:38 +0000

    cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: string1
+ * @src: string2
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
