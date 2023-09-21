e65d781e83e8cf6fb931846f9862bfc3c78ec2e8
Mistire <mdwithgod@gamail.com>
2023-09-21 08:49:29 +0000

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,23 +2,23 @@
 /**
  * cap_string - capitalizes all words of a string
  * @c: array of characters
- * Return: c
+ * Return: str
  */
-char *cap_string(char *c)
+char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (c[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (c[i] == ' ' || c[i] == '.' || c[i] == '\t' || c[i] == '\n')
+		if (str[i] == ' ' || str[i] == '.' || str[i] == '\t' || str[i] == '\n')
 		{
-			if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				c[i + 1] -= 32;
+				str[i + 1] -= 32;
 			}
 		}
 		i++;
 	}
 
-	return (c);
+	return (str);
 }
