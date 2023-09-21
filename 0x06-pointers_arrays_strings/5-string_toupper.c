069e42aba3ee1540c2ffb320209f0a547031a926
Mistire <mdwithgod@gamail.com>
2023-09-21 08:03:18 +0000

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#inclde "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @c: char
+ * Return: ptr
+ */
+char *string_toupper(char *c)
+{
+	char *ptr = c;
+
+	while (c != '\0')
+	{
+		if (c >= 'a' || c <= 'z')
+		{
+			*c -= ('a' - 'A');
+		}
+		c++;
+	}
+	return (ptr);
+}
