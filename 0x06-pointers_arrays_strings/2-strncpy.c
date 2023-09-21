8d48e0e1f8537e8cb7570eb57fc2016e1d499ef6
Mistire <mdwithgod@gamail.com>
2023-09-21 06:45:37 +0000

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy -  copies a string
+ * @dest: string1
+ * @src: string2
+ * @n: integer
+ * Return: ptr
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (n > 0 && *src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--
+	}
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+
+	return (ptr);
+}
