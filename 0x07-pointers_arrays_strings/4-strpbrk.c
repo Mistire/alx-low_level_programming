8e6a469ba00fbd472ae48be0459a3469c8c0f37c
Mistire <mdwithgod@gamail.com>
2023-09-25 20:11:00 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+/**
+ * _strpbrk - string for any of a set of bytes
+ * @s: string
+ * @accept: bytrs
+ * Return: pointer or null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
